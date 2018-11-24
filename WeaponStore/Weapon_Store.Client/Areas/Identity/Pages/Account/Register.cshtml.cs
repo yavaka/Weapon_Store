5c3ad39b184ea54b7e19d22c4a92e8386170ebc6
yavaka <yavakaa@gmail.com>
2018-11-24 05:30:01 +0000

    Update nav-bar

--- WeaponStore/Weapon_Store.Client/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WeaponStore/Weapon_Store.Client/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -63,7 +63,7 @@ namespace Weapon_Store.Client.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Identity/Account/Login");
             if (ModelState.IsValid)
             {
                 var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
